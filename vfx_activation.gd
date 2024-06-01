d32efed1b13b14b5b4227abe7c3b5643554a922e
mino9421 <mino9421@gmail.com>
2024-06-01 15:41:09 -0400

    created repo

---
+++ vfx_activation.gd
@@ -0,0 +1,27 @@
+extends Node3D
+
+var explosion_particles: Array
+var testing_particles: GPUParticles3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	explosion_particles = [
+		$vfx_explosion_orange/Sparks,
+		$vfx_explosion_orange/Flash,
+		$vfx_explosion_orange/Fire,
+		$vfx_explosion_orange/Smoke
+	]
+	testing_particles = $vfx_explosion_orange/Sparks
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+func _input(event):
+	if event.is_action_pressed("activate_vfx"):
+		for particle in explosion_particles:
+			if particle:
+				particle.emitting = true
+			else:
+				print("Explosion particles not found or in effect.")
